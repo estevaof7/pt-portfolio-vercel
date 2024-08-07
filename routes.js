9329e16e30fe31625e85c8be2ffada5a3aa919c0
Estêvão Ferreira <estevaofset@hotmail.com>
2024-08-07 13:19:29 -0300

    tinha esquecido de mudar a variável isEnglish

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 route.get('/', (req, res) => {
     
-    const isEnglish = true;
+    const isEnglish = false;
     
     res.render('index', {
         isEnglish: isEnglish
