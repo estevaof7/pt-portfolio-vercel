535ffa79e11d17cd12a8a97cbc0b3a22f7174fe2
Estêvão Ferreira <estevaofset@hotmail.com>
2024-10-17 15:08:06 -0300

    Carrossel adicionado

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const app = express();
 
 const router = require('./routes');
 
-app.use(express.json()); //NÃO SEI SE VOU PRECISAR
+app.use(express.json());
 
 const path = require('path');
-// app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.static(path.resolve(__dirname, 'frontend')));
-// app.use(express.static(path.resolve(__dirname, '..', 'src', 'views')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs'); 
 
-app.use(router); //
-app.listen(3000, () => { //
+app.use(router); 
+app.listen(3000, () => { 
     console.log('Servidor executando na porta 3000: http://localhost:3000')
 });
